264b64fd3acf46a436e740fe1619c516573f6db0
Renato Benatti <rbenatti@uolinc.com>
2020-08-25 21:53:56 -0300

    more slices tests example

--- slice/sum.go
+++ slice/sum.go
@@ -1,10 +1,34 @@
 package slice
 
-// Sum receives an array of 5 numbers and resumes their sum
+// Sum receives an slice of numbers and return sum
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+// SumAll receives one or more slices of numbers and return one or more sums
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+// SumAllRest receives one or more slices of numbers and return one or more sums of each final
+func SumAllRest(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+			continue
+		}
+
+		final := numbers[1:]
+		sums = append(sums, Sum(final))
+	}
+	return sums
 }
--- slice/sum_test.go
+++ slice/sum_test.go
@@ -1,6 +1,7 @@
 package slice
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -14,10 +15,44 @@ func TestSum(t *testing.T) {
 		}
 	})
 }
+func TestSumAll(t *testing.T) {
+	result := SumAll([]int{1, 2}, []int{0, 9})
+	expect := []int{3, 9}
+	if !reflect.DeepEqual(result, expect) {
+		t.Errorf("expect '%v' - result '%v'", expect, result)
+	}
+}
+
+func TestSumAllRest(t *testing.T) {
+	verifySums := func(t *testing.T, result, expect []int) {
+		t.Helper()
+		if !reflect.DeepEqual(result, expect) {
+			t.Errorf("expect '%v' - result '%v'", expect, result)
+		}
+	}
+
+	t.Run("makes the sums of some slices", func(t *testing.T) {
+		result := SumAllRest([]int{1, 2}, []int{0, 9})
+		expect := []int{2, 9}
+		verifySums(t, result, expect)
+	})
+
+	t.Run("sum empty slices securely", func(t *testing.T) {
+		result := SumAllRest([]int{}, []int{3, 4, 5})
+		expect := []int{0, 9}
+		verifySums(t, result, expect)
+	})
+}
 
 func BenchmarkSum(b *testing.B) {
 	numbers := []int{1, 2, 3, 4, 5}
 	for i := 0; i < b.N; i++ {
 		_ = Sum(numbers)
+	}
+}
+
+func BenchmarkSumAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = SumAll([]int{1, 2}, []int{0, 9})
 	}
 }
